133dd62136eb1028758dc69f7815958205037b5e
tomfuertes <tomfuertes@gmail.com>
2026-01-07 14:40:08 -0600

    Display checked in builders count from BatchRegistry contract

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,8 +3,13 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
+  const { data: checkedInCount } = useScaffoldReadContract({
+    contractName: "BatchRegistry",
+    functionName: "checkedInCounter",
+  });
   return (
     <>
       <div className="flex items-center flex-col grow pt-10">
@@ -16,7 +21,7 @@ const Home: NextPage = () => {
           <p className="text-center text-lg">Get started by taking a look at your batch GitHub repository.</p>
           <p className="text-lg flex gap-2 justify-center">
             <span className="font-bold">Checked in builders count:</span>
-            <span>To Be Implemented</span>
+            <span>{checkedInCount?.toString() || "0"}</span>
           </p>
         </div>
 
